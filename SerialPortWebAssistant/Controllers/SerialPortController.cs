c1b20f0bf62d30bf6eafba0b9d48791469805ef9
3gbywork <614258809@qq.com>
2019-05-30 16:30:33 +0800

    add serial port controller

---
+++ SerialPortWebAssistant/Controllers/SerialPortController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SerialPortWebAssistant.Controllers
+{
+    public class SerialPortController : Controller
+    {
+        // GET: SerialPort
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
