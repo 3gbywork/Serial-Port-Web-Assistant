e10ddf245cbbffe5496dfe085b394ad52b5ec3e9
3gbywork <614258809@qq.com>
2019-06-09 16:30:33 +0800

    add hub

---
+++ SerialPortWebAssistant/SignalRHubs/SerialPortHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SerialPortWebAssistant.SignalRHubs
+{
+    [HubName("serialPortHub")]
+    public class SerialPortHub : Hub
+    {
+
+    }
+}
