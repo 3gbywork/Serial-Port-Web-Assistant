7e0570c2e54d124c4cd3ce42650da4c861a44111
3gbywork <614258809@qq.com>
2019-06-15 16:30:33 +0800

    add serial port extension

---
+++ SerialPortWebAssistant/Extensions/SerialPortExtension.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace SerialPortWebAssistant.Extensions
+{
+    static public class SerialPortExtension
+    {
+        public static bool TryOpen(this SerialPort serialPort)
+        {
+            if (null == serialPort)
+                return false;
+            if (serialPort.IsOpen)
+                return true;
+
+            try
+            {
+                serialPort.Open();
+            }
+            catch (Exception)
+            {
+            }
+
+            return serialPort.IsOpen;
+        }
+
+        public static bool TryClose(this SerialPort serialPort)
+        {
+            if (null == serialPort)
+                return false;
+            if (!serialPort.IsOpen)
+                return true;
+
+            try
+            {
+                serialPort.DiscardOutBuffer();
+                serialPort.DiscardInBuffer();
+                serialPort.Close();
+            }
+            catch (Exception)
+            {
+            }
+
+            return serialPort.IsOpen;
+        }
+    }
+}
