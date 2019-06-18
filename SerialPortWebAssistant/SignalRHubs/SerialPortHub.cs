f10e8f909f2f6ce6e1bcce7a84ac5b143cfbeafa
3gbywork <614258809@qq.com>
2019-06-18 16:30:33 +0800

    add hub functions

--- SerialPortWebAssistant/SignalRHubs/SerialPortHub.cs
+++ SerialPortWebAssistant/SignalRHubs/SerialPortHub.cs
@@ -1,15 +1,35 @@
 ﻿using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.SignalR.Hubs;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using SerialPortWebAssistant.Managers;
+using SerialPortWebAssistant.Models;
 
 namespace SerialPortWebAssistant.SignalRHubs
 {
     [HubName("serialPortHub")]
     public class SerialPortHub : Hub
     {
+        public SerialPortHub() : this(SerialPortManager.Instance) { }
 
+        public SerialPortHub(SerialPortManager manager)
+        {
+            SerialPort = manager;
+        }
+
+        private SerialPortManager SerialPort { get; set; }
+
+        public bool Open(SerialPortModel model)
+        {
+            return SerialPort.Open(model);
+        }
+
+        public bool Close(SerialPortModel model)
+        {
+            return SerialPort.Close(model);
+        }
+
+        public void SendMessage(SerialPortModel model)
+        {
+            SerialPort.SendMessage(model);
+        }
     }
 }
