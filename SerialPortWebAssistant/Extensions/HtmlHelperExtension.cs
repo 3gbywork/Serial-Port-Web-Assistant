24de3954414c9d00a2508e81f87a89199219045f
3gbywork <614258809@qq.com>
2019-06-20 16:30:33 +0800

    add html helper extension add resource manager extension

---
+++ SerialPortWebAssistant/Extensions/HtmlHelperExtension.cs
@@ -0,0 +1,14 @@
+﻿using Resources;
+using System.Web.Mvc;
+
+namespace SerialPortWebAssistant.Extensions
+{
+    public static class HtmlHelperExtension
+    {
+        public static string LocalizationFor(this HtmlHelper htmlHelper, string key)
+        {
+            SerialPort.ResourceManager.TryGetString(key, out string value);
+            return value;
+        }
+    }
+}
---
+++ SerialPortWebAssistant/Extensions/ResourceManagerExtension.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+using System.Resources;
+using System.Threading;
+
+namespace SerialPortWebAssistant.Extensions
+{
+    public static class ResourceManagerExtension
+    {
+        public static bool TryGetString(this ResourceManager resourceManager, string key, CultureInfo culture, string defaultValue, out string value)
+        {
+            value = defaultValue;
+            try
+            {
+                value = resourceManager.GetString(key, culture);
+            }
+            catch
+            {
+                //ignore exception and return the defaultValue
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool TryGetString(this ResourceManager resourceManager, string key, out string value)
+        {
+            return resourceManager.TryGetString(key, Thread.CurrentThread.CurrentUICulture, key, out value);
+        }
+    }
+}
