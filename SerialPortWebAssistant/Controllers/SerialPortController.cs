f0d63daf5d3120e3a00476e7033d6316b4d10160
3gbywork <614258809@qq.com>
2019-07-22 16:30:33 +0800

    update controller

--- SerialPortWebAssistant/Controllers/SerialPortController.cs
+++ SerialPortWebAssistant/Controllers/SerialPortController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using SerialPortWebAssistant.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -11,7 +13,35 @@ namespace SerialPortWebAssistant.Controllers
         // GET: SerialPort
         public ActionResult Index()
         {
-            return View();
+            ViewBag.Title = Resources.SerialPort.Serial_Port_Web_Assistant;
+
+            var model = new SerialPortModel
+            {
+                SelectBaudRate = 19200,
+                QuickCommandList = GetQuickCommandList(),
+            };
+            return View("SerialPort", model);
+        }
+
+        private IEnumerable<SelectListItem> GetQuickCommandList()
+        {
+            var commands = new List<SelectListItem>();
+
+            const string filename = "~/App_Data/CEVT串口命令.txt";
+            var path = Server.MapPath(filename);
+            if (System.IO.File.Exists(path))
+            {
+                var lines = System.IO.File.ReadAllLines(path, Encoding.GetEncoding("gb2312"));
+                commands = lines.Select(line =>
+                {
+                    var strs = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    if (strs.Length == 2)
+                        return new SelectListItem { Text = strs[0], Value = strs[1] };
+                    return null;
+                }).Where(item => null != item).ToList();
+            }
+
+            return commands;
         }
     }
 }
