5a95cbb7cb3805e98ab46c76cb1fc6cdf74c146e
3gbywork <614258809@qq.com>
2019-07-23 16:30:33 +0800

    update model

--- SerialPortWebAssistant/Models/SerialPortModel.cs
+++ SerialPortWebAssistant/Models/SerialPortModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.IO.Ports;
 using System.Linq;
@@ -15,19 +16,19 @@ namespace SerialPortWebAssistant.Models
         public IEnumerable<SelectListItem> StopBitsList { get; set; }
         public IEnumerable<SelectListItem> QuickCommandList { get; set; }
 
-        [Display(Name = "串口号")]
+        [Display(Name = "Select_Serial_Port", ResourceType = typeof(Resources.SerialPort))]
         public string SelectSerialPort { get; set; }
-        [Display(Name = "波特率")]
+        [Display(Name = "Select_Baud_Rate", ResourceType = typeof(Resources.SerialPort))]
         public int SelectBaudRate { get; set; }
-        [Display(Name = "数据位")]
+        [Display(Name = "Select_Data_Bits", ResourceType = typeof(Resources.SerialPort))]
         public int SelectDataBits { get; set; }
-        [Display(Name = "校验位")]
+        [Display(Name = "Select_Parity", ResourceType = typeof(Resources.SerialPort))]
         public string SelectParity { get; set; }
-        [Display(Name = "停止位")]
+        [Display(Name = "Select_Stop_Bits", ResourceType = typeof(Resources.SerialPort))]
         public string SelectStopBits { get; set; }
-        [Display(Name = "接收区")]
+        [Display(Name = "Receive_Area", ResourceType = typeof(Resources.SerialPort))]
         public string Receive { get; set; }
-        [Display(Name = "发送区")]
+        [Display(Name = "Send_Area", ResourceType = typeof(Resources.SerialPort))]
         public string Send { get; set; }
 
         public bool IsOpened { get; set; }
@@ -37,8 +38,18 @@ namespace SerialPortWebAssistant.Models
             SerialPortList = SerialPort.GetPortNames().Select(name => new SelectListItem { Text = name, Value = name });
             BaudRateList = new List<int> { 115200, 76800, 57600, 43000, 38400, 19200, 9600, 4800, 2400, 1200 }.Select(baud => new SelectListItem { Text = baud.ToString(), Value = baud.ToString() });
             DataBitsList = new List<int> { 8, 7, 6, 5 }.Select(bits => new SelectListItem { Text = bits.ToString(), Value = bits.ToString() });
-            ParityList = new List<string> { "无", "奇校验", "偶校验" }.Select(parity => new SelectListItem { Text = parity, Value = parity });
-            StopBitsList = new List<string> { "0", "1", "1.5", "2" }.Select(bits => new SelectListItem { Text = bits, Value = bits });
+            ParityList = new Dictionary<string, Parity>
+            {
+                { Resources.SerialPort.None_Parity, Parity.None },
+                { Resources.SerialPort.Odd_Parity, Parity.Odd },
+                { Resources.SerialPort.Even_Parity, Parity.Even }
+            }.Select(parity => new SelectListItem { Text = parity.Key, Value = parity.Value.ToString() });
+            StopBitsList = new Dictionary<string, StopBits>
+            {
+                { "1", StopBits.One },
+                { "1.5", StopBits.OnePointFive },
+                { "2", StopBits.Two }
+            }.Select(bits => new SelectListItem { Text = bits.Key, Value = bits.Value.ToString() });
             QuickCommandList = new List<SelectListItem>();
 
             IsOpened = false;
@@ -48,27 +59,16 @@ namespace SerialPortWebAssistant.Models
 
         public Parity GetParity()
         {
-            if ("奇校验" == SelectParity)
-                return Parity.Odd;
-            else if ("偶校验" == SelectParity)
-                return Parity.Even;
-            else
-                return Parity.None;
+            if (Enum.TryParse(SelectParity, out Parity parity))
+                return parity;
+            return Parity.None;
         }
 
         public StopBits GetStopBits()
         {
-            switch (SelectStopBits)
-            {
-                case "0":
-                    return StopBits.None;
-                case "1.5":
-                    return StopBits.OnePointFive;
-                case "2":
-                    return StopBits.Two;
-                default:
-                    return StopBits.One;
-            }
+            if (Enum.TryParse(SelectStopBits, out StopBits stopBits))
+                return stopBits;
+            return StopBits.One;
         }
     }
 }
